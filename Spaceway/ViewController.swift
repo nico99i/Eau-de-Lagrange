8aa9bad24030662d52299fb2c8640792ae72c607
Matteo Melloni <matteomelloni8@gmail.com>
2019-10-19 14:30:55 +0200

    Initial Commit

---
+++ Spaceway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spaceway
+//
+//  Created by Matteo Melloni on 19/10/2019.
+//  Copyright © 2019 Matteo Melloni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
